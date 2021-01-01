a16509fde590cca10c904f8cfabfafe12495d9c3
Patricia Rico <prico@transerv.local>
2021-01-01 23:17:05 +0100

    funciones de activación y desactivación

--- wp-content/plugins/prueba/prueba.php
+++ wp-content/plugins/prueba/prueba.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* Plugin Name: My Basics Plugin
-* Plugin URI: https://example.com/plugins/the-basics/
-* Description: Handle the basics with this plugin.
+* Plugin Name: Pruebas
+* Plugin URI: https://srttorres.com
+* Description: Este es mi primer plugin. 
 * Version: 1.10.3
 * Requires at least: 5.2
 * Requires PHP: 7.2
@@ -13,3 +13,18 @@
 * Text Domain: my-basics-plugin
 * Domain Path: /languages
 */
+
+function res_install(){
+    //Accion
+    require_once 'activador.php';
+}
+//__FILE__ es el archivo donde nos encontramos
+register_activation_hook(__FILE__, 'res_install');
+
+
+function res_desactivacion(){
+    //Accion
+    flush_rewrite_rules();
+    //podría eliminar una tabla de la base de datos
+}
+register_deactivation_hook(__FILE__, 'res_desactivacion');
